a4487335040748b1ac617659bd271d1ee232a9ba
Evelyn Avila <67525832+EliKasher@users.noreply.github.com>
2025-09-08 20:53:21 -0300

    Versión final de estructura D:

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router';
+import CourseForm from './frontend/components/Courses';
+
 
 function Home() {
   return (
