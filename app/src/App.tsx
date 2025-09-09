4e349b382bf11255f81a28e5667dfb210a834a65
Evelyn Avila <67525832+EliKasher@users.noreply.github.com>
2025-09-08 21:27:56 -0300

    Ahora si que si

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,27 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router';
 import CourseForm from './frontend/components/Courses';
 
+function InscriptionForm() {
+  return (
+    <>
+      <div className="course-register">
+        <div className="register-header">
+          <h1>Formulario Inscripción EdV Verano 2026</h1>
+          <h4>Este formulario pretende optimizar y sistematizar los programas e información asociadas a los cursos EdV.</h4>
+          <h4 className="register-precaution">
+            Recuerda que todos los campos marcados con
+            <span className="required"></span> son obligatorios.
+            </h4>
+        </div>
+        <div className="course-form-container">
+          <form className="course-form">
+
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
 
 function Home() {
   return (
@@ -15,11 +36,11 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="register-course" element={<CourseForm />}></Route>
+        <Route path="register-course" element={<InscriptionForm />}></Route>
       </Routes>
     </div>
     </>
   )
 }
 
-export default App
+export default App;
