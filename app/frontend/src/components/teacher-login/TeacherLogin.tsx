a646a023408e37dac334847e181647faa0b97a4a
Tomás Méndez <tomas.mendez@ug.uchile.cl>
2025-10-13 17:25:42 -0300

    changed text in teacher login

--- app/frontend/src/components/teacher-login/TeacherLogin.tsx
+++ app/frontend/src/components/teacher-login/TeacherLogin.tsx
@@ -51,7 +51,7 @@ const TeacherLogin = () => {
           </div>
 
           <button type="submit" className="submit-btn">
-            Guardar
+            Entrar
           </button>
         </div>
       </div>
