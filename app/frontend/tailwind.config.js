59a37054b1c0b2e245548b8b62d9c0f22a837537
Evelyn Avila <67525832+EliKasher@users.noreply.github.com>
2025-11-27 11:42:09 -0300

    FIX: Outline cambiado, al fin no es negro

---
+++ app/frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        function({ addBase }) {
+        addBase({
+            'button:focus': {
+            outline: 'none',
+            boxShadow: 'none',
+            },
+            'button:focus-visible': {
+            outline: '2px solid #3b82f6',
+            outlineOffset: '2px',
+            },
+        })
+        }
+    ],
+    }
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
   resolve: {
     alias: {
